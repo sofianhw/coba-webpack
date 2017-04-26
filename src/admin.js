5322d06c29710bba9a5b5a4fbbc8267a09cebb6c
sofian hadiwijaya <reztinpeace@gmail.com>
2017-04-27 02:10:33 +0700

    bersih2 master

--- src/admin.js
+++
@@ -1,4 +0,0 @@
-import people from './people'
-
-const root = document.querySelector('#root')
-root.innerHTML = `<p>There are ${people.length} people.</p>`
--- src/app.js
+++
@@ -1,8 +0,0 @@
-import './style.scss'
-import {groupBy} from 'lodash/collection'
-import people from './people'
-
-const managerGroups = groupBy(people, 'manager')
-
-const root = document.querySelector('#root')
-root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`
